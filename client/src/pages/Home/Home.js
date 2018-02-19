261aa19fcfc970d4fad82d46b12185b2d6cd6559
Caitlin Barron <barroncn@gmail.com>
2018-02-19 18:36:10 +0000

    outline home page

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Home.css";
+import Nav from "../../components/Nav";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}

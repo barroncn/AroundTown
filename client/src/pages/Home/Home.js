303f71b96d7ff74d48a81e455ff0584816484a62
Caitlin Barron <barroncn@gmail.com>
2018-02-20 18:53:11 +0000

    search bar added to home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -7,6 +7,17 @@ class Home extends Component {
         return (
             <div>
                 <Nav />
+                <br />
+                <br />
+                <div className="container">
+                    <div className="input-group">
+                          <input type="text" className="form-control searchbar" placeholder="Enter a city to find events and activities nearby..." />
+                          <span className="input-group-btn">
+                            <button className="btn btn-secondary" type="button">Go!</button>
+                          </span>
+                       
+                    </div>
+                </div>
             </div>
         );
     }

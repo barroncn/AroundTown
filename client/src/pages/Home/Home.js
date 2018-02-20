4896f26528ecbdcb8e607c74fbde308270460d37
Caitlin Barron <barroncn@gmail.com>
2018-02-20 19:12:49 +0000

    background fix

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -15,7 +15,7 @@ class Home extends Component {
                         </div>
                         <div className="col-md-6">
                             <div className="input-group d-inline-flex p-3">
-                                  <input type="text" className="form-control" placeholder="Enter a city to find events and activities nearby..." />
+                                  <input type="text" className="form-control" placeholder="Enter a city..." />
                                   <span className="input-group-btn">
                                     <button className="btn btn-secondary" type="button">Go!</button>
                                   </span>

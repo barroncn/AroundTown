49ad723f1175a9dfe949696a8ebe70684406366d
Caitlin Barron <barroncn@gmail.com>
2018-02-20 18:22:03 +0000

    add background image, format navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
 
 class Nav extends Component {
-    render() {
-        return (
-            <nav className="nav">
-                <a className="nav-link" href="#">Around Town</a>
-                <ul className="nav justify-content-end mr-auto">
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Login</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Register</a>
-                  </li>
-                </ul>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+                <nav className="navbar navbar-inverse">
+                    <a className="navbar-brand text-white ml-1" href="/"><h3>Around Town</h3></a>
+                    <ul className="nav justify-content-end">
+                      <li className="nav-item">
+                        <a className="nav-link text-white" href="/login">Login</a>
+                      </li>
+                     <li className="nav-item">
+                        <a className="nav-link text-white" href="/signup">Sign Up</a>
+                      </li>
+                    </ul>
+                </nav>
+            </div>
+    );
+  }
 }
 
 export default Nav;
